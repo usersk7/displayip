52163856a4fed3f0e829a334b34f3b339e6e92f7
owner <owner@DESKTOP-IETQ8UL.mshome.net>
2017-10-27 22:56:56 +0530

    new comit

---
+++ src/displayip/DisplayIP.java
@@ -0,0 +1,11 @@
+
+package displayip;
+import java.net.InetAddress;
+public class DisplayIP {
+
+    public static void main(String[] args) throws Exception{
+        System.out.println(InetAddress.getLocalHost());
+        // TODO code application logic here
+    }
+    
+}
